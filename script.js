6cd81dbf7008684674d1d4a023a1be20a2b9f33a
ahmed <warhunter69@gmail.com>
2021-12-20 01:07:26 +0200

    5

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 class todo{
     
-    constructor(title,discreption){
+    constructor(title,discreption,project){
 
         this.title = title;
         this.discreption = discreption;
         this.status = false;
-
+        this.project = project;
     }
     get title(){
         return this._title;
@@ -15,6 +15,12 @@ class todo{
     }
     get status(){
         return this._status;
+    }
+    get project(){
+        return this._project;
+    }
+    set project(value){
+        this._project = value;
     }
     set title(value){
         this._title = value;
@@ -28,10 +34,18 @@ class todo{
 }
 class project{
     
-    constructor(todo_list = []){
+    constructor(todo_list = [],title){
+        this.title = title ; 
         this.todo_list = todo_list;
-
+      
     }
+    get title(){
+        return this._title;
+    }
+    set title(value){
+        this._title = value;
+    }
+    
     
     set todo_list(value){
         this._todo_list = value
@@ -50,7 +64,7 @@ function add_todo(){
     const discreption = document.querySelector('#input_discreption').value;
 
     const todo_item = new todo(title,discreption);
-
+    
     
     todo_list.push(todo_item);
     console.log(todo_list)
@@ -85,13 +99,48 @@ function acordion(){
 
 document.addEventListener('DOMContentLoaded', function() {
     
-    const submit_button = document.querySelector('#submit_todo');
-    submit_button.addEventListener('click', ()=>{
+    const submit_todo = document.querySelector('#submit_todo');
+    submit_todo.addEventListener('click', ()=>{
         add_todo();
         acordion();
     });
- 
-   
+    const submit_project = document.querySelector('#submit_project');
+    submit_project.addEventListener('click',()=>{
+           
+        add_project();
+        listProjects();
+    })
+    const defultProject = new project([],"Defult");
+    project_list.push(defultProject);
+    listProjects();
+    
 });
 let todo_list = [];
 let project_list = [];
+function add_project(){
+    const title = document.querySelector('#input_project').value;
+    const newProject = new project([],title);
+    project_list.push(newProject);
+    console.log(project_list)
+}
+function listProjects(){
+    const ul = document.querySelector('#ulProject');
+    ul.innerHTML = ''
+    for(let i = 0 ; i < project_list.length ; i++){
+        let li = document.createElement('li');
+        let button = createProjectButton(project_list[i].title);
+        li.append(button);
+        ul.append(li);
+    }
+    
+}
+function createProjectButton(projectName){
+   
+    const button = document.createElement('button');
+    button.className = "btn btn-primary";
+    button.style.margin = "3px";
+    button.textContent = projectName;
+    return button;
+
+}
+function listTodo(){}
